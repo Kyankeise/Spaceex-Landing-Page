bd6ed0fcb69f15d3f5e97d9dbac7d0f8f78e755a
Kyankeise <122470088+Kyankeise@users.noreply.github.com>
2023-09-19 12:13:15 +0100

    Update main.js

--- spaceex-website/js/main.js
+++ spaceex-website/js/main.js
@@ -2,15 +2,20 @@
 // Date: 15/11/2022
 // Title: SpaceX project
 
+// Declare menu button, overlay mobile menu and counter variables
+
 const btn = document.getElementById('menu-btn');
 const overlay = document.getElementById('overlay');
 const menu = document.getElementById('mobile-menu');
 const counters = document.querySelectorAll('.counter');
 let scrollStarted = false;
 
+// Listen for a click to activate navToggle
+
 btn.addEventListener('click', navToggle);
 document.addEventListener('scroll', scrollPage);
 
+// Function which controls the side navigation Menu. Toggle open class to change Hamburger menu from 3 lines to X. Show Overlay class. Stop scroll when the overlay class showed
 
 function navToggle() {
     btn.classList.toggle('open');
@@ -29,6 +34,8 @@ function scrollPage() {
         scrollStarted = false;
     }
 }
+
+// For each loop to Increment the 3 numbers on the Falcon page
 
 function countUp() {
     counters.forEach((counter) => {
@@ -57,6 +64,8 @@ function countUp() {
         updateCounter();
     });
 }
+
+// Resets the 3 counters on the Falcon page
 
 function reset() {
     counters.forEach((counter) => counter.innerHTML = '0');
